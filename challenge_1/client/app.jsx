3bb3529ba2709815d7a68e473985c6bddd39888e
Max Proano <proanom89@gmail.com>
2021-05-25 00:16:48 -0700

    Complete webpack and react setup

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+  return <h1>Historical Events Finder</h1>
+};
 
+ReactDOM.render(<App/>, document.getElementById('root'));
