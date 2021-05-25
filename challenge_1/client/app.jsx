e3456016af3870c053ee5cbd04ae344b9e1e3525
Max Proano <proanom89@gmail.com>
2021-05-25 14:44:25 -0700

    Complete retrieving and rendering the events

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Events from './components/Events.jsx';
 
 const App = () => {
-  return <h1>Historical Events Finder</h1>
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    axios.get('/events')
+      .then(res => {
+        setEvents(res.data);
+      })
+      .catch();
+  }, []);
+
+  return (
+    <>
+      <h1>Historical Events Finder</h1>
+      <Events events={events}/>
+    </>
+  )
 };
 
 ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ challenge_1/client/components/Event.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Event = ({ event }) => {
+  return <div>{event.description}</div>
+};
+
+export default Event;
