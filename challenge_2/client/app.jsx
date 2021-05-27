fa5f1313949aa91232a7da5cd8f91e17ab4d5d64
Max Proano <proanom89@gmail.com>
2021-05-26 19:20:39 -0700

    Setup webpack and react

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+  return (
+    <h1>Cryptocurrency Charting Tool</h1>
+  )
+};
+
+ReactDOM.render(<App/>, document.getElementById('chart'));
 
