8e18a37528d61da63b08feb621f3b1ae1d9ae4bc
Max Proano <proanom89@gmail.com>
2021-05-27 12:27:18 -0700

    Complete BMR for challenge_2

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,10 +5,6 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
