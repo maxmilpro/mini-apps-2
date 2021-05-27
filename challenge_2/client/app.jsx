09e443a08ded61abb6a6e430d2206dcc44a0c446
Max Proano <proanom89@gmail.com>
2021-05-26 22:57:18 -0700

    Complete data fetch

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const App = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.coindesk.com/v1/bpi/historical/close.json');
+        console.log(response.data);
+        setData(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <h1>Cryptocurrency Charting Tool</h1>
   )
