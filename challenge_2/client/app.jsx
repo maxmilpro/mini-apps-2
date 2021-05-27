c8c8d271dec3f3709c11711fc66b5406ebbc941b
Max Proano <proanom89@gmail.com>
2021-05-27 12:26:40 -0700

    Complete rendering time-series chart w/ api data

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import TimeSeries from './components/TimeSeries.jsx';
 
 const App = () => {
-  const [data, setData] = useState({});
-  useEffect(() => {
+  const [data, setData] = useState([]);
+  const [labels, setLabels] = useState([]);
 
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://api.coindesk.com/v1/bpi/historical/close.json');
-        console.log(response.data);
-        setData(response.data);
+        const bpi = response.data.bpi;
+        setData(Object.values(bpi));
+        setLabels(Object.keys(bpi));
       } catch (err) {
         console.log(err);
       }
@@ -20,7 +23,11 @@ const App = () => {
   }, []);
 
   return (
-    <h1>Cryptocurrency Charting Tool</h1>
+    <>
+      <h1>Cryptocurrency Charting Tool</h1>
+      <TimeSeries data={data} labels={labels}/>
+      <div>Powered by <a href="https://www.coindesk.com/price/bitcoin">CoinDesk</a></div>
+    </>
   )
 };
 
