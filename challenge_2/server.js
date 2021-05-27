ba77d75591c4aa98502430877b20b2bb11627d0f
Max Proano <proanom89@gmail.com>
2021-05-26 19:20:14 -0700

    Setup simple express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
