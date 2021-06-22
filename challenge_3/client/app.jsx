f88b25164bfe7228409e0a265fe38392da4bfe67
Max Proano <proanom89@gmail.com>
2021-06-22 14:16:33 -0700

    Complete webpack and react setup

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+  return (
+    <h2>this is react</h2>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
 
