e82375f8a2bc7bc0a128f5be3b08d32c6d55b2a8
Max Proano <proanom89@gmail.com>
2021-06-24 22:22:51 -0700

    Add calculateScores helper to app.jsx

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Scorecard from './components/Scorecard.jsx';
 import PinSelector from './components/PinSelector.jsx';
+import calculateScores from './components/helpers.jsx';
 
 const App = () => {
   const [rolls, setRolls] = useState([]);
@@ -29,28 +30,7 @@ const App = () => {
   }, [rolls]);
 
   useEffect(() => {
-    let score = 0;
-    const newScores = [];
-    for (let i = 0; i < frames.length; i ++) {
-      const currentFrame = frames[i];
-      const firstRoll = currentFrame[0];
-      const secondRoll = currentFrame[1];
-      const thirdRoll = currentFrame[2];
-      const [firstBonusRoll, secondBonusRoll] = ([frames[i + 1], frames[i + 2]]).flat();
-      // debugger;
-      if (firstRoll === 10 && currentFrame.length === 1) {
-        score += (firstRoll + (firstBonusRoll || 0) + (secondBonusRoll || 0));
-      } else if (firstRoll === 10 && currentFrame.length === 3) {
-        score += (firstRoll + secondRoll + thirdRoll);
-      } else if (firstRoll + secondRoll === 10 && currentFrame.length === 3) {
-        score += (firstRoll + secondRoll + thirdRoll);
-      } else if (firstRoll + secondRoll === 10) {
-        score += (10 + (firstBonusRoll || 0));
-      } else {
-        score += (firstRoll + secondRoll);
-      }
-      newScores.push(score);
-    }
+    const newScores = calculateScores(frames);
     setScores(newScores);
     setTotalScore(newScores[newScores.length - 1]);
   }, [frames]);
