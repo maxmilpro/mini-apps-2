e5b91e01085982a74fb05d002fa2d02b28b78875
Max Proano <proanom89@gmail.com>
2021-06-30 11:53:07 -0700

    Add ability to flag squares

--- challenge_4/src/features/minesweeper/components/Square.js
+++ challenge_4/src/features/minesweeper/components/Square.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleVisibility, findVisibleSquares} from '../minesweeperSlice';
 import styles from '../Minesweeper.module.css';
@@ -7,6 +7,7 @@ const Square = ({ square, y, x }) => {
   const {value, visible} = square;
   const board = useSelector(state => state.minesweeper.board);
   const dispatch = useDispatch();
+  const [flagged, setFlagged] = useState(false);
 
   let displayText;
   if (visible === false) {
@@ -19,16 +20,21 @@ const Square = ({ square, y, x }) => {
 
   return (
     <div
-      className={styles.square}
-      onClick={() => {
-        if (value === 'X') {
-          dispatch(toggleVisibility({ y, x }))
-          alert('Game Over!')
-        } else {
-          dispatch(findVisibleSquares({ board, y, x }))
+      className={`${styles.square} ${flagged ? styles.flag : ''} ${visible ? styles.visible : ''}`}
+      onClick={(e) => {
+          if (value === 'X') {
+            dispatch(toggleVisibility({ y, x }))
+            alert('Game Over!')
+          } else {
+            dispatch(findVisibleSquares({ board, y, x }))
+          }
         }
       }
-    }>{displayText}</div>
+      onContextMenu={(e) => {
+        e.preventDefault()
+        setFlagged(!flagged);
+      }}
+    >{displayText}</div>
   )
 };
 
